f8397d78eceff3ea1d0c2e24ace3b68f59165cd6
rebs-code <jack.rebussi@gmail.com>
2024-02-07 15:49:27 +0100

    feat: display projects in table at index.blade.php

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -7,5 +7,29 @@
                 <li>{{ $project->name }}</li>
             @endforeach
         </ul>
+        <table class="table-striped-columns table table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Project Name</th>
+                    <th scope="col">Tech Stack</th>
+                    <th scope="col">Repo Link</th>
+                    <th scope="col">Slug</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>{{ $project->name }}</td>
+                        <td>{{ $project->tech_stack }}</td>
+                        <td>{{ $project->repo_link }}</td>
+                        <td>{{ $project->slug }}</td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
     </div>
 @endsection
