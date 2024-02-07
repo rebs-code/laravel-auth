c0c450d4ba54911e45b141c7597866b8d3d3d5bd
rebs-code <jack.rebussi@gmail.com>
2024-02-07 15:28:39 +0100

    add: Projects link to admin navigation

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -63,6 +63,13 @@
                                     <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Dashboard
                                 </a>
                             </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link text-white {{ Route::currentRouteName() == 'dashboard' ? 'bg-secondary' : '' }}"
+                                    href="{{ route('admin.projects.index') }}">
+                                    <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Projects
+                                </a>
+                            </li>
                         </ul>
 
 
